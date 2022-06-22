4ea7d366af950319353a37963fde3bf89f9e09a3
ir2guru <ir2guru@hotmail.com>
2022-06-22 09:14:53 +0100

    sec

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -1,6 +1,13 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, PureComponent } from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faL, faMicrophone, faPhone} from '@fortawesome/free-solid-svg-icons';
+import {faVideoCamera} from '@fortawesome/free-solid-svg-icons';
+import {faDesktop} from '@fortawesome/free-solid-svg-icons';
 import io from "socket.io-client";
 
+
+var RoomStatus = "Guest Yet To Join";
+var inichiator = false;
 const Room = (props) => {
     const userVideo = useRef();
     const partnerVideo = useRef();
@@ -8,22 +15,42 @@ const Room = (props) => {
     const socketRef = useRef();
     const otherUser = useRef();
     const userStream = useRef();
+    const screenTrack = useRef();
     const senders = useRef([]);
+    const [state, setState] = useState(false);
+    const [camstate, csetState] = useState(false);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMCounter] = useState(0);
 
     useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ audio: false, video: true }).then(stream => {
+        const interval = setInterval(() => {
+            setSeconds((seconds) => seconds + 1);
+            //setminute();
+           // console.log("GGG :", "jjj");
+          }, 1000);
+
+        if(seconds > 10){
+            console.log("GGG :", "jjj");
+        }
+
+
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
             userVideo.current.srcObject = stream;
             userStream.current = stream;
+            MuteAudioStart();
 
             socketRef.current = io.connect("/");
             socketRef.current.emit("join room", props.match.params.roomID);
 
             socketRef.current.on('other user', userID => {
                 callUser(userID);
+                inichiator = true;
+                RoomStatus = "Guest Yet To Join";
                 otherUser.current = userID;
             });
 
             socketRef.current.on("user joined", userID => {
+                 RoomStatus = "Guest Joined";
                 otherUser.current = userID;
             });
 
@@ -34,10 +61,22 @@ const Room = (props) => {
             socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
         });
 
+        return () => clearInterval(interval);
     }, []);
 
+    if(minutes < 30){
+         if(seconds === 59){
+        setMCounter((minutes) => minutes + 1);
+        setSeconds((seconds) => 0);
+        console.log("Done :","True");
+    }
+    }else{
+        EndMyCall();
+    }
+   
     function callUser(userID) {
         peerRef.current = createPeer(userID);
+
         userStream.current.getTracks().forEach(track => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
     }
 
@@ -57,6 +96,7 @@ const Room = (props) => {
         peer.onicecandidate = handleICECandidateEvent;
         peer.ontrack = handleTrackEvent;
         peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+        
 
         return peer;
     }
@@ -90,12 +130,14 @@ const Room = (props) => {
                 sdp: peerRef.current.localDescription
             }
             socketRef.current.emit("answer", payload);
-        })
+        });
+
     }
 
     function handleAnswer(message) {
         const desc = new RTCSessionDescription(message.sdp);
         peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
+        RoomStatus = "";
     }
 
     function handleICECandidateEvent(e) {
@@ -119,31 +161,71 @@ const Room = (props) => {
         partnerVideo.current.srcObject = e.streams[0];
     };
 
+    function handleScreenShareStopped(e){
+        senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
+    };
+
     function shareScreen() {
         navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
             const screenTrack = stream.getTracks()[0];
             senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
-            screenTrack.onended = function() {
-                senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
-            }
+            screenTrack.onended = handleScreenShareStopped;
         })
+    };
+
+    function MuteAudioStart(){
+        setState(!state);
+        const audiotrackss = userStream.current.getTracks().find(track => track.kind === 'audio');
+        //console.log("Called", videotrackss.enabled);
+        audiotrackss.enabled = state;
+        console.log("Sound State", audiotrackss.enabled);
+        console.log("bgColor :", state);
     }
 
-    return ( <
-        div >
-        <
-        video controls style = {
-            { height: 500, width: 500 }
-        }
-        autoPlay ref = { userVideo }
-        /> <
-        video controls style = {
-            { height: 500, width: 500 }
-        }
-        autoPlay ref = { partnerVideo }
-        /> <
-        button onClick = { shareScreen } > Share screen < /button> < /
-        div >
+    const MuteAudio=() =>{
+        setState(!state);
+        const audiotrackss = userStream.current.getTracks().find(track => track.kind === 'audio');
+        //console.log("Called", videotrackss.enabled);
+        audiotrackss.enabled = state;
+        console.log("Sound State", audiotrackss.enabled);
+        console.log("bgColor :", state);
+    };
+
+    function EndMyCall(){
+        csetState(!camstate);
+        const videotrackss = userStream.current.getTracks().find(track => track.kind === 'video');
+        videotrackss.enabled = camstate;
+        userStream.current.getTracks().forEach(t => t.stop());
+        peerRef.current.close();
+        window.location.href = "https://google.com/contact";
+    }
+    const EnableVideo=()=>{
+        csetState(!camstate);
+        const videotrackss = userStream.current.getTracks().find(track => track.kind === 'video');
+        videotrackss.enabled = camstate;
+        console.log("video bgColor :", camstate);
+    };
+
+
+    return (
+        <body>
+        <div  className="bg">
+            <p className="msg">{RoomStatus}</p>
+            <video controls style={{height: 500, width: 500}} autoPlay ref={userVideo} />
+            <video controls style={{height: 500, width: 500}} autoPlay ref={partnerVideo} />
+            <div className="buttons">
+            <button className="btn" style={{backgroundColor: 'red'}} onClick={EndMyCall}><FontAwesomeIcon icon={faPhone} ></FontAwesomeIcon></button>
+                 <button className="btn" style={{backgroundColor: state ? 'red' : 'green'}} onClick={MuteAudio}><FontAwesomeIcon icon={faMicrophone} ></FontAwesomeIcon></button>
+                 <button className="btn" style={{backgroundColor: camstate ? 'red' : 'green'}} onClick={EnableVideo}><FontAwesomeIcon icon={faVideoCamera} ></FontAwesomeIcon></button>
+                 <button className="btn" onClick={shareScreen}><FontAwesomeIcon icon={faDesktop} ></FontAwesomeIcon></button>
+                 <p className="time">Call Duration: {minutes} : {seconds < 10 ? `0${seconds}` : seconds}</p>
+                 <p className="note">Please Note Call Ends in 30mins</p>
+            </div>
+            <br></br>
+           
+        </div> 
+        </body> 
+
     );
 };
 
